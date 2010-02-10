d0f4a71a65b05a5d5d3675d4728bfdbc4cac0453
ragnargisli <ragnargisli@de3fd216-0748-11df-8861-21795b59afe9>
2010-02-10 02:56:00 +0000

    Controller is singleton,  minimum functionality added.

--- trunk/kguide/src/bravo/kguide/control/Controller.java
+++ trunk/kguide/src/bravo/kguide/control/Controller.java
@@ -1,5 +1,35 @@
 package bravo.kguide.control;
 
+import bravo.kguide.control.RouteList;
+import bravo.kguide.data.ServerConnection;
+import java.util.ArrayList;
+
+
 public class Controller {
+    public RouteList routeList = new RouteList();
+    public ServerConnection server;
+    
+    //Singleton creation of control
+    protected Controller() {}  // private so other classes can't instantiate 
+    static private Controller INSTANCE = null;
+    
+    /**
+     * @return The unique instance of this class.
+     */
+    static public Controller getInstance() {  
+	if(null == INSTANCE) {
+	    INSTANCE = new Controller();
+	}
+	return INSTANCE;
+    }
+    //Singleton part ends
+
+    public void initData() {
+	routeList.addRoute(server.getRouteOnServer(3));
+	routeList.addRoute(server.getRouteOnServer(4));
+	routeList.addRoute(server.getRouteOnServer(5));
+	routeList.addRoute(server.getRouteOnServer(6));
+	
+    }
 
 }
