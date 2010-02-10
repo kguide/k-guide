2a8bad03bc607ffc3fc40738fd9394e45334afa9
ragnargisli <ragnargisli@de3fd216-0748-11df-8861-21795b59afe9>
2010-02-10 02:52:51 +0000

    Code for drawing routes added in googleMapScreen.  Template for mapdrawing added too(MapWidgets) so that the onDraw in mapOverlay wont get too cluttered.

---
+++ trunk/kguide/src/bravo/kguide/view/MapWidgets.java
@@ -0,0 +1,38 @@
+package bravo.kguide.view;
+
+import bravo.kguide.control.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.graphics.Point;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.graphics.Canvas; 
+import android.graphics.Paint; 
+import android.graphics.Bitmap; 
+import android.graphics.BitmapFactory; 
+import android.graphics.Paint.Style; 
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class MapWidgets {
+    
+}
