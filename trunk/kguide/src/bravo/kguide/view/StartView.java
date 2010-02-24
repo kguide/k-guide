33fd86ab57df24b9c976d5e5d646f899b6c8359b
hordurg83 <hordurg83@de3fd216-0748-11df-8861-21795b59afe9>
2010-02-24 04:40:10 +0000

    Added a view design. Includes imagebuttons with different state for normal - focused or pushed "actions". Not all buttons are connected with activity at the moment.

--- trunk/kguide/src/bravo/kguide/view/StartView.java
+++ trunk/kguide/src/bravo/kguide/view/StartView.java
@@ -4,19 +4,62 @@ package bravo.kguide.view;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class StartView extends Activity {
     
-     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_view);
-	Intent startGoogleMapScreen = new Intent(StartView.this,GoogleMapScreen.class);
-	startActivity(startGoogleMapScreen);
-    }
+        setContentView(R.layout.main_v);
+                
+	    ImageButton continuePrevious = (ImageButton)findViewById(R.id.v_main_b01_continue);
+	    
+	    
+	    continuePrevious.setOnClickListener(new ImageButton.OnClickListener(){
+	    	public void onClick(View v){
+	    		Intent startGoogleMapScreen = new Intent(StartView.this,GoogleMapScreen.class);
+	    		startActivity(startGoogleMapScreen);    			
+	    	}
+	    
+	    });
+	    
+	    ImageButton selectRoute = (ImageButton)findViewById(R.id.v_main_b02_selectroute);
+	    selectRoute.setOnClickListener(new ImageButton.OnClickListener(){
+	    	public void onClick(View v){
+	    		; //does nothing at the moment
+	    	}
+	    });
+	    
+	    ImageButton showMap = (ImageButton)findViewById(R.id.v_main_b03_showmap);
+	    showMap.setOnClickListener(new ImageButton.OnClickListener(){
+	    	public void onClick(View v){
+	    		; //does nothing at the moment
+	    	}
+	    });
+	    
+	    ImageButton settings = (ImageButton)findViewById(R.id.v_main_b04_settings);
+	    settings.setOnClickListener(new ImageButton.OnClickListener(){
+	    	public void onClick(View v){
+	    		; //does nothing at the moment
+	    	}
+	    });
+	    
+	    ImageButton help = (ImageButton)findViewById(R.id.v_main_b05_help);
+	    help.setOnClickListener(new ImageButton.OnClickListener(){
+	    	public void onClick(View v){
+	    		; //does nothing at the moment
+	    	}
+	    });
+	
+	    
+	    ImageButton quit = (ImageButton)findViewById(R.id.v_main_b06_quit);
+	    quit.setOnClickListener(new ImageButton.OnClickListener(){
+	    	public void onClick(View v){
+	    		finish();
+	    	}
+	    });
+
+    } //onCreate closing
 }
