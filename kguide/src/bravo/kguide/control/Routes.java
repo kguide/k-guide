ad095cd21ceb1d5d406a03bdcfc9215ff29a03da
hordurg83 <hordurg83@de3fd216-0748-11df-8861-21795b59afe9>
2010-02-25 18:26:31 +0000

    Inserted a few constants for different types of media

--- kguide/src/bravo/kguide/control/Routes.java
+++ kguide/src/bravo/kguide/control/Routes.java
@@ -3,33 +3,37 @@ package bravo.kguide.control;
 import java.util.ArrayList;
 import com.google.android.maps.GeoPoint;
 
-
-
 public class Routes {
 
-    
+	
+	public final int MEDIA_TEXT = 1;
+	public final int MEDIA_PHOTO = 2;
+	public final int MEDIA_URL = 3;
+	public final int MEDIA_AUDIO = 4;
+	
+	
     public int routeId;
     public String routeName;
     public ArrayList<Coordinate> routePath = new ArrayList<Coordinate>();
     
-    
-    
     public Routes(int id, String name) {
-	this.routeId = id;
-	this.routeName = name;
+		this.routeId = id;
+		this.routeName = name;
     }
-    
     
     public void addCoordinate(double currentLatitude, double currentLongitude) {
-	routePath.add(new Coordinate(currentLatitude, currentLongitude));
+    	routePath.add(new Coordinate(currentLatitude, currentLongitude));
     }
 
-    public class Coordinate {
-	
-	public GeoPoint p;	
-	public Coordinate(double latitude, double longitude){
-	    p = new GeoPoint((int) (latitude * 1E6),(int) (longitude * 1E6));
-	}
+   
+    
+    public class Coordinate {	
+    	public GeoPoint p;	
+    	
+    	public Coordinate(double latitude, double longitude){
+		    p = new GeoPoint((int) (latitude * 1E6),(int) (longitude * 1E6));
+		    
+    	}
     }
 }
 
